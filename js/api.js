d02706e60d3fb7f8761333aa9838cf792293dbc9
Cao Thanh My <caothanhmy@gmail.com>
2026-05-22 20:45:43 +0700

    aa

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-// Khởi tạo Supabase Client (Sếp thay URL và Key của sếp vào đây)
+// Khởi tạo Supabase Client
 const SUPABASE_URL = 'https://tngrpulkrdvlijougzba.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRuZ3JwdWxrcmR2bGlqb3VnemJhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0NDg5NzMsImV4cCI6MjA5NTAyNDk3M30.w35WQJRd9lMb6mVsF0Ikwysyp1iZGHZMEwLoK7GBEtM';
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
@@ -30,7 +30,8 @@ const API = {
     async saveProduct(product) {
         const { data, error } = await supabase.from('products').upsert(product);
         return { data, error };
-    }
+    }, // <-- Dấu phẩy cực kỳ quan trọng ở đây!
+
     // 3. QUẢN LÝ UPLOAD ẢNH
     async uploadImage(file, folder = 'products') {
         const fileExt = file.name.split('.').pop();
@@ -53,4 +54,3 @@ const API = {
         return data.publicUrl;
     }
 };
-
